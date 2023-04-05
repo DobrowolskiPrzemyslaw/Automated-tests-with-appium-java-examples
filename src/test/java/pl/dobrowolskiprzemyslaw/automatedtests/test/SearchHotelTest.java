e79944fe3544567e9be228487ae80456ff742820
DobrowolskiPrzemyslaw <dobrowolski.przemyslaw.1991@gmail.com>
2023-04-05 14:25:04 +0200

    Added DataProvider to tests

--- src/test/java/pl/dobrowolskiprzemyslaw/automatedtests/test/SearchHotelTest.java
+++ src/test/java/pl/dobrowolskiprzemyslaw/automatedtests/test/SearchHotelTest.java
@@ -5,18 +5,20 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pl.dobrowolskiprzemyslaw.automatedtests.pages.*;
+import pl.dobrowolskiprzemyslaw.automatedtests.utils.AppiumHelper;
 
 import java.util.List;
 
 public class SearchHotelTest extends BaseTest {
-    @Test
+    @Test(dataProvider = "data")
     @Feature("Feature")
     @Story("Story")
     @Description("Description")
-    public void searchHotel() {
+    public void searchHotel(String cityName,String startDate,String returnnDate) {
         LoginPage lp = new LoginPage();
         HomePage hp = new HomePage();
         SearchPage sp = new SearchPage();
@@ -26,13 +28,13 @@ public class SearchHotelTest extends BaseTest {
 
         lp.login();
         hp.focusOnSearchInput();
-        sp.searchCity("London");
-        cp.inputTravelDate("25 April 2023","01 May 2023");
+        sp.searchCity(cityName);
+        cp.inputTravelDate(startDate,returnnDate);
         hp.changeTraveler();
-        sradp.addRoom();
-        sradp.addAdult();
-        sradp.addAdult();
-        sradp.subtractAdult();
+        sradp.addRoom(2);
+        sradp.subtractRoom(1);
+        sradp.addAdult(2);
+        sradp.subtractAdult(1);
         sradp.addChild();
         sradp.confirmChanges();
         Assert.assertTrue(driver.findElement(MobileBy.id("com.booking:id/facet_entry_point_item_label")).isDisplayed());
@@ -42,6 +44,15 @@ public class SearchHotelTest extends BaseTest {
         softAssert.assertEquals(labelsList.get(0),"Sort");
         softAssert.assertEquals(labelsList.get(1),"Filter");
         softAssert.assertEquals(labelsList.get(2),"Map");
+        AppiumHelper.takeScreenshot();
         softAssert.assertAll();
+    }
+
+    @DataProvider(name = "data")
+    public Object[][] dataProvider(){
+        return new Object[][] {
+                {"Roma","25 April 2023","15 May 2023"},
+                {"London","26 April 2023","20 May 2023"},
+                {"Warsaw","27 April 2023","25 May 2023"}};
     }
 }
